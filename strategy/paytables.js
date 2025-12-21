39cc20f82e56bc6ce628a0e9a3350e67f836b3a8
aymenpack <azimaymen@gmail.com>
2025-12-21 16:44:21 -0700

    Create paytables.js

---
+++ strategy/paytables.js
@@ -0,0 +1,34 @@
+// 6/5 Bonus Poker base pays
+export const BASE_PAY_65 = {
+  RF: 800,
+  SF: 50,
+  K4A: 80,
+  K42_4: 40,
+  K45K: 25,
+  FH: 6,
+  FL: 5,
+  ST: 4,
+  K3: 3,
+  TP: 2,
+  JOB: 1,
+  N: 0
+};
+
+// Ultimate X award multipliers (Triple Play, Bonus Poker 6/5)
+export const AWARD_MULT_3PLAY = {
+  RF: 2,
+  SF: 2,
+  K4A: 2,
+  K42_4: 2,
+  K45K: 2,
+  FH: 12,
+  FL: 11,
+  ST: 8,
+  K3: 4,
+  TP: 3,
+  JOB: 2,
+  N: 1
+};
+
+// Ultimate X Progressive Royal
+export const UXP_ROYAL = 4000;
